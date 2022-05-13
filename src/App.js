76ecc5940b888719ba281142a425279e4e314ed8
ejioguvictor <ejioguvictor6054@gmail.com>
2022-05-13 09:10:38 +0100

    created angularank task

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MemberCard from "./components/card/card";
+
+function App() {
+  return (
+    <div>
+      <MemberCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/card/card.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {
+  Box,
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+  CardMedia,
+} from "@mui/material";
+
+function MemberCard() {
+  return (
+    <Box width="400px">
+      <Card>
+        <CardMedia
+          component="img"
+          height={300}
+          image="https://source.unsplash.com/random"
+          alt="unsplash image"
+        ></CardMedia>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Contributors
+          </Typography>
+          <Typography variant="body2" color="white">
+            <h3>Followers</h3>
+            <h3>Repos</h3>
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Details</Button>
+        </CardActions>
+      </Card>
+    </Box>
+  );
+}
+
+export default MemberCard;
